46e415244949842f6d4991b7bbf12b015b6a3f1d
ottokap <ottokap@metropolia.fi>
2019-12-13 07:11:13 +0200

    'cookie'

--- app.js
+++ app.js
@@ -13,3 +13,24 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get('/setCookie', function(req, res){
+  res.cookie('color', 'red').send('cookie set');
+});
+
+app.get('/deleteCookie', function(req, res){
+  res.clearCookie('color');
+  res.send('cookie cleared');
+});
+
+app.get('/form', (req, res) => {
+  res.render('views/form.pug');
+});
+
+app.get('/secret', (req, res) => {
+  res.render('views/secret.pug');
+});
+
+app.post('/login', (req, res) => {
+  res.render('');
+});
